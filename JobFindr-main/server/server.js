e44fad80f46fcbe9cc77461852a7f665f8db15ed
Harshit-53 <harshitsuryawanshi7233@gmail.com>
2025-02-11 17:51:10 +0530

    auth0 correction

--- JobFindr-main/server/server.js
+++ JobFindr-main/server/server.js
@@ -29,7 +29,7 @@ const config = {
   session: {
     absoluteDuration: 30 * 24 * 60 * 60 * 1000, // 30 days
     cookie: {
-      domain: "jobfindr-q1cl.onrender.com",
+      domain: "skillsync-1vl3.onrender.com",
       secure: true,
       sameSite: "None",
     },
